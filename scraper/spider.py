015c12e25615b95320a0cc362a427c78f9546be4
Omar Trejo <otrenav@gmail.com>
2018-01-18 14:49:41 -0600

    Handle errors and save them to disk (in progress)

--- scraper/spider.py
+++ scraper/spider.py
@@ -1,5 +1,10 @@
 
+import pandas
 import scrapy
+
+from twisted.internet.error import TimeoutError, TCPTimedOutError
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
 
 from utilities.inputs import SearchStringsCSV
 from utilities.select import select
@@ -14,10 +19,20 @@ class Spider(scrapy.Spider):
     }
 
     def __init__(self, settings={}, **kwargs):
+        self.failed_urls = []
         self.name = settings.name
         self.site_settings = settings
         self.start_urls = self._start_urls()
         super().__init__(**kwargs)
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url,
+                callback=self.parse,
+                errback=self._handle_error,
+                dont_filter=True
+            )
 
     def parse(self, response):
         key = self.site_settings.first_item_selectors_key
@@ -26,6 +41,15 @@ class Spider(scrapy.Spider):
             if first_item:
                 yield response.follow(first_item, self._parse_item)
                 break
+
+    def handle_spider_closed(self, spider, reason):
+        print('!' * 50)
+        print(self.failed_urls)
+        print('!' * 50)
+        # TODO: Move into it's own class
+        data = pandas.DataFrame(self.failed_urls)
+        f = "outputs/{}_{}_errors.{}".format(self.name, "0000-00-00", "csv")
+        data.to_csv(f, columns=["url", "error"])
 
     def _parse_item(self, response):
         yield Item(response, self.site_settings).data()
@@ -38,3 +62,16 @@ class Spider(scrapy.Spider):
 
     def _search_strings(self):
         return SearchStringsCSV(self.site_settings).search_strings()
+
+    def _handle_error(self, response):
+        print('*' * 50)
+        self.logger.error(repr(response))
+        if response.check(HttpError):
+            error = 'HTTP Error {}'.format(response.status)
+        elif response.check(DNSLookupError):
+            error = 'DNS Lookup Error'
+        elif response.check(TimeoutError, TCPTimedOutError):
+            error = 'TCP Timeout Error'
+        self.failed_urls.append([response.value.response.url, error])
+        self.logger.error(error)
+        print('*' * 50)
