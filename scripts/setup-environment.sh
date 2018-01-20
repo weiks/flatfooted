e7cb0f556fb46dcd2e2ac63b0bb4c3197d0f4c1d
Omar Trejo <otrenav@gmail.com>
2018-01-20 04:23:15 -0600

    Add Chromium installation to environment setup

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get install python3-pip libssl-dev -y
+sudo apt-get install python3-pip libssl-dev chromium-browser -y
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install -r requirements.txt
