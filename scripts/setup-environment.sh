d31a178998eb375a3fdb17c4c14f13ce5b3908a0
Omar Trejo <otrenav@gmail.com>
2018-01-19 18:30:47 -0600

    Add flag to avoid the warnings when using `sudo`

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install python3-pip libssl-dev -y
-sudo pip3 install --upgrade pip
-sudo pip3 install -r requirements.txt
+sudo pip3 -H install --upgrade pip
+sudo pip3 -H install -r requirements.txt
