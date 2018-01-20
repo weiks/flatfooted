56a4f6d6167cf72c6781f17ed05872e1d498a7ac
Omar Trejo <otrenav@gmail.com>
2018-01-19 18:28:35 -0600

    Remove unnecessary comment

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -1,10 +1,3 @@
-#
-# Setup Ubuntu environment for scraping
-#
-# NOTE: This does not ensure Python 3, extra care must be taken.
-#       If necessary, add steps to install virtualenvwrapper,
-#       and create a Python 3 environment.
-#
 sudo apt-get update
 sudo apt-get install python-pip3 libssl-dev -y
 sudo pip3 install -r requirements.txt
