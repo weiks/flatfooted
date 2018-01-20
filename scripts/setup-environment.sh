da20355ca8d6765d050364c37e4d7ec21f2d1bd7
Omar Trejo <otrenav@gmail.com>
2018-01-19 18:31:23 -0600

    Fix flag location

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install python3-pip libssl-dev -y
-sudo pip3 -H install --upgrade pip
-sudo pip3 -H install -r requirements.txt
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install -r requirements.txt
