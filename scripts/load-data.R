5b59f0a37e73e5c3b68e793f8ab708a895d99611
Omar Trejo <otrenav@gmail.com>
2018-01-22 13:40:18 -0600

    Load site data into R data frame

---
+++ scripts/load-data.R
@@ -0,0 +1,38 @@
+#
+# Load site data into data frame
+#
+
+FILE_EXTENSION <- "csv"
+OUTPUTS_DIRECTORY <- "../outputs/"
+
+full_file_name <- function(file) {
+    paste(OUTPUTS_DIRECTORY, file, sep = "")
+}
+
+load_site_data <- function(site) {
+    print("Loading CSV files...")
+    files <- list.files(OUTPUTS_DIRECTORY, pattern = "*.csv")
+    filter <- site %in% files
+    data <- NULL
+    for (file in files) {
+        full_file_name <- full_file_name(file)
+        print(paste("   ", full_file_name))
+        data <- rbind(data, read.csv(full_file_name))
+    }
+    print("Done.")
+    return(data)
+}
+
+#
+# Usage example
+#
+# NOTE: Data needs post-processing for analysis,
+#       but this is how you can load it.
+#
+
+site <- "Amazon"
+
+amazon <- load_site_data(site)
+
+str(amazon)
+head(amazon)
