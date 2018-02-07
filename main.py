2c49591aa634fa0576f6f5aa9aeaf8c609487b88
Omar Trejo <otrenav@gmail.com>
2018-02-06 18:23:15 -0600

    Specify how to schedule jobs in Ubuntu with `cron`

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 
-# from utilities.scheduler import Scheduler
 from scraper.scraper import Scraper
 from settings import SETTINGS
 
 
 def main():
-    print("[+] Ready...")
     scraper = Scraper(SETTINGS)
     scraper.start()
-
-    # When implementing scheduled scraping, this code will
-    # come in handy. The implementation is practically
-    # finished, but I still need to test it, and I did
-    # not have time to do it for this time.
-    #
-    # scheduler = Scheduler().get_scheduler()
-    # for site in SITES:
-    #     scheduler.add_job(
-    #         site.scrape(async=True),
-    #         "cron", day="*/1", timezone="America/Mexico_City")
-    # scheduler.start()
 
 
 if __name__ == "__main__":
