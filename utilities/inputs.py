a0ba758232e5ace27f38a781207ca7511031aeeb
Omar Trejo <otrenav@gmail.com>
2018-02-11 02:56:10 -0600

    Fix sampling error when sites don't have enough search strings

--- utilities/inputs.py
+++ utilities/inputs.py
@@ -15,7 +15,6 @@ class SearchStringsCSV:
         use = data[s.use_on].str.contains(s.name, na=False)
         all = data[s.use_on].str.contains('ALL', na=False)
         strings = list(data[s.search_string][(use | all) & ~avoid])
-        sample = s.search_sample
-        if sample:
-            strings = random.sample(strings, sample)
+        if s.search_sample and len(strings) > s.search_sample:
+            strings = random.sample(strings, s.search_sample)
         return strings
